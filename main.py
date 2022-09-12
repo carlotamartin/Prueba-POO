01bb96114798177dee0452fcc4eabe4f43151afa
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-09-12 11:25:40 +0200

    primer apartado

--- main.py
+++ main.py
@@ -6,19 +6,20 @@ class Vehiculo():
         return "Color {}, {} ruedas".format( self.color, self.ruedas )
 
 class Coche(Vehiculo):
-    def __init__(self, color, ruedas, velocidad, cilindrada):
-        Vehiculo.__init__(self, color, ruedas)
+    def __init__(self, color, ruedas,velocidad, cilindrada):
+        super().__init__(color, ruedas)
         self.velocidad = velocidad
         self.cilindrada = cilindrada
     def __str__(self):
         return Vehiculo.__str__(self) + ", {} km/h, {}cc".format(self.velocidad, self.cilindrada)
 
 
+
 #Creamos la clase Camioneta que hereda de coche
 
 class Camioneta(Coche):
     def __init__(self, color, ruedas, velocidad, cilindrada, carga):
-        Coche.__init__(self, color, ruedas, velocidad, cilindrada)
+        super().__init__(self, color, ruedas, velocidad, cilindrada)
         self.carga = carga
 
     def __str__(self):
@@ -27,23 +28,36 @@ class Camioneta(Coche):
 #Creamos la clase Bicicleta que hereda de Vehiculo
 
 class Bicicleta(Vehiculo):
-    def __init__(self, color, ruedas, ):
-        Vehiculo.__init__(self, color, ruedas)
-        self.velocidad = velocidad
-        self.cilindrada = cilindrada
+    def __init__(self, color, ruedas, tipo):
+        super().__init__( color, ruedas)
+        if self.ruedas > 8:
+            self.tipo = 'deportivo'
+        else:
+            self.tipo = 'urbano'
     def __str__(self):
-        return Vehiculo.__str__(self) + ", {} km/h, {}cc".format(self.velocidad, self.cilindrada)
+        return Vehiculo.__str__(self) + ", es de tipo {}".format(self.tipo)
+
+
 
 #Creamos la clase Motocicleta que hereda de Bicicleta
 class Motocicleta(Bicicleta):
-    def __init__(self, color, ruedas, velocidad, cilindrada):
-        Bicicleta.__init__(self, color, ruedas)
+    def __init__(self, color, ruedas, tipo, velocidad, cilindrada):
+        super().__init__( color, ruedas, tipo)
         self.velocidad = velocidad
         self.cilindrada = cilindrada
     def __str__(self):
         return Bicicleta.__str__(self) + ", {} km/h, {}cc".format(self.velocidad, self.cilindrada)
 
+
 c = Coche("azul", 4, 150, 1200)
-print(c)
+f = Coche ('rojo', 5, 300, 1400)
+print(c,f)
+j= Bicicleta('azul', 2, 20)
+print(j)
+h = Motocicleta('azul', 9, 5, 100, 200)
+print(h)
+
+lista = [c, f, j, h]
+
 
 
