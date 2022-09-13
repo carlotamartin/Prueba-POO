9f7885c6074c029799dee22107b6435c9f26f949
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-09-13 11:34:29 +0200

    fin del ejercicio

--- Bicicleta.py
+++ Bicicleta.py
@@ -1,10 +1,11 @@
 from Vehiculo import Vehiculo
 
 
+#Si pones que el tipo es mayor que 3, es de tipo deportivo porque es de gama muy alta.
 class Bicicleta(Vehiculo):
     def __init__(self, color, ruedas, tipo):
         super().__init__( color, ruedas)
-        if self.ruedas > 3:
+        if tipo > 3:
             self.tipo = 'deportivo'
         else:
             self.tipo = 'urbano'
--- main.py
+++ main.py
@@ -1,68 +1,8 @@
-class Vehiculo():
-    def __init__(self, color, ruedas):
-        self.color = color
-        self.ruedas = ruedas
-    def __str__(self):
-        return "Color {}, {} ruedas".format( self.color, self.ruedas )
-
-    def get_ruedas (self):
-        return int(self.ruedas)
-
-class Coche(Vehiculo):
-    def __init__(self, color, ruedas,velocidad, cilindrada):
-        super().__init__(color, ruedas)
-        self.velocidad = velocidad
-        self.cilindrada = cilindrada
-    def __str__(self):
-        return Vehiculo.__str__(self) + ", {} km/h, {}cc".format(self.velocidad, self.cilindrada)
-
-    def get_ruedas (self):
-        return int(self.ruedas)
-
-#Creamos la clase Camioneta que hereda de coche
-
-class Camioneta(Coche):
-    def __init__(self, color, ruedas, velocidad, cilindrada, carga):
-        super().__init__(self, color, ruedas, velocidad, cilindrada)
-        self.carga = carga
-
-    def __str__(self):
-        return Coche.__str__(self) + ", {} kg".format(self.carga)
-
-    def get_ruedas (self):
-        return int(self.ruedas)
-
-#Creamos la clase Bicicleta que hereda de Vehiculo
-
-class Bicicleta(Vehiculo):
-    def __init__(self, color, ruedas, tipo):
-        super().__init__( color, ruedas)
-        if self.ruedas > 3:
-            self.tipo = 'deportivo'
-        else:
-            self.tipo = 'urbano'
-    def __str__(self):
-        return Vehiculo.__str__(self) + ", es de tipo {}".format(self.tipo)
-
-    def get_ruedas (self):
-        return int(self.ruedas)
-
-
-
-#Creamos la clase Motocicleta que hereda de Bicicleta
-class Motocicleta(Bicicleta):
-    def __init__(self, color, ruedas, tipo, velocidad, cilindrada):
-        super().__init__( color, ruedas, tipo)
-        self.velocidad = velocidad
-        self.cilindrada = cilindrada
-    def __str__(self):
-        return Bicicleta.__str__(self) + ", {} km/h, {}cc".format(self.velocidad, self.cilindrada)
-
-    def get_ruedas (self):
-        return int(self.ruedas)
-
-
-
+from Camioneta import Camioneta
+from Motocicleta import Motocicleta
+from Coche import Coche
+from Bicicleta import Bicicleta
+from Vehiculo import Vehiculo
 
 #Creamos el método catalogar
 def catalogar(lista):
@@ -70,6 +10,7 @@ def catalogar(lista):
         print (str(type(i).__name__ )+ ' = '+ str(i) )
 
 
+#Creamos el segundo método de catalogar
 def catalogar_2 (lista, ruedas):
     j = 0
     for i in lista:
@@ -83,11 +24,12 @@ def main():
     #Creamos los distintos vehículos
     c = Coche("azul", 4, 150, 1200)
     f = Coche ('rojo', 5, 300, 1400)
-    j= Bicicleta('azul', 2, 20)
+    j= Bicicleta('azul', 2, 2)
     h = Motocicleta('azul', 4, 5, 100, 200)
+    a = Camioneta('azul', 6, 100, 100, 3000)
 
     #Creamos la lista de vehículos
-    lista = [c, f, j, h]
+    lista = [c, f, j, h, a]
 
     catalogar(lista)
 
